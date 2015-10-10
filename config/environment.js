ef91a74278e94dba86bdc5cd1a522d0164b275d8
pathway27 <pathway27@gmail.com>
2015-10-10 23:58:46 +1100

    chore(env): add more google related domains for csp

--- config/environment.js
+++ config/environment.js
@@ -23,9 +23,9 @@ module.exports = function(environment) {
       'script-src':  "'self' 'unsafe-inline' 'unsafe-eval' apis.google.com storage.googleapis.com",
       'font-src':    "'self' data: fonts.gstatic.com",
       'style-src':   "'self' 'unsafe-inline' storage.googleapis.com fonts.googleapis.com",
-      'frame-src':   "'self' 'unsafe-inline' accounts.google.com",
+      'frame-src':   "'self' 'unsafe-inline' accounts.google.com content.googleapis.com",
       'connect-src': "'self' localhost:3000",
-      'img-src':     "'self' s3.amazonaws.com lorempixel.com csi.gstatic"
+      'img-src':     "'self' s3.amazonaws.com lorempixel.com csi.gstatic apis.google.com"
     },
   };
 

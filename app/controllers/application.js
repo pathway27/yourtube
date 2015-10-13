974ab26379df96ad6f96881fb312a5ccc6146e0a
pathway27 <pathway27@gmail.com>
2015-10-13 23:51:13 +1100

    feat(auth): correctly redirects to and from when authenticated
    Also copying what was in index template to subscriptions template.
    How it is supposed to be.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,5 +3,13 @@ import Ember from "ember";
 export default Ember.Controller.extend({
   yourtube: Ember.inject.service('yourtube-api'),
 
-  authenticated: Ember.computed.alias('yourtube.authenticated')
+  authenticated: Ember.computed.alias('yourtube.authenticated'),
+
+  authChanged: Ember.observer('authenticated', function() {
+    var auth = this.get('authenticated');
+
+    if (auth) {
+      this.transitionToRoute('subscriptions');
+    }
+  })
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,14 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
+  // Scope out to Authenticated Mixin
+  beforeModel() {
+    var auth = this.get('yourtube.authenticated');
+
+    if (!auth)
+      this.transitionTo('index');
+  },
+
   actions: {
     gapi () {
       this.get('yourtube').oauth();

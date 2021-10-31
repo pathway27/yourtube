abc338ca74c0e084be3d7cb73a15a1667dfe2aab
pathway27 <pathway27@gmail.com>
2021-10-31 17:21:46 +0100

    feat: subs on index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,7 @@
 import ApplicationController from './application';
 
+import { tracked } from '@glimmer/tracking';
+
 export default class IndexController extends ApplicationController {
+  @tracked model;
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import ApplicationRoute from './application';
 
+import { inject as service } from '@ember/service';
+
 export default class IndexRoute extends ApplicationRoute {
-};
+  @service store
+
+  model() {
+    return this.store.findAll('subscription');
+  }
+}
